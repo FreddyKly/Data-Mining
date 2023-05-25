624448d79c493407756d156f0c6b5cbbb5858e62
FreddyKly <frederik.klie@gmail.com>
2023-05-25 14:56:07 +0200

    f)

--- Sprint_2/booklet_2.r
+++ Sprint_2/booklet_2.r
@@ -1,3 +1,6 @@
+print(airquality)
+print(nrow(airquality))
+
 print("Ozon Column:")
 print(airquality$Ozon)
 
@@ -5,6 +8,26 @@ mean <- mean(airquality$Ozon)
 print("Ozon Column Mean:")
 print(mean)
 
+#b)
 missing_values <- is.na(airquality$Ozon)
 print("Ozon Column missing values:")
 print(missing_values)
+
+#c
+num_missing_values <- sum(missing_values)
+print("How many values are 'NA'")
+print(num_missing_values)
+
+#d
+mean_no_na <- mean(airquality$Ozon, na.rm = TRUE)
+print("Ozon Column Mean without 'NA':")
+print(mean_no_na)
+
+#e)
+airquality_no_na <- na.omit(airquality)
+print("Airquality datafram without 'NA'")
+print(airquality_no_na)
+print(nrow(airquality_no_na))
+
+#f)
+print(complete.cases(airquality$Ozone))
