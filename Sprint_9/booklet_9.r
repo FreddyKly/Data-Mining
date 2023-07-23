ebf31f5a6613b2386d6cb4cbde94e26994dbf811
FreddyKly <Frederik.Kliemt@web.de>
2023-07-23 19:13:49 +0200

    9 done

---
+++ Sprint_9/booklet_9.r
@@ -0,0 +1,22 @@
+library(nnet)
+library(randomForest)
+
+churn = read.csv(file="C:/Users/fredd/Documents/Data-Mining/Sprint_8/Data/churn.txt")
+n = nrow(churn)
+set.seed(42)
+train.id = sample(n, size = 0.7*n)
+test.id = setdiff(1:n, train.id)
+trainingdata=churn[train.id,]
+validationdata=churn[test.id,]
+churnfactor=factor(train$Churn.)#
+train = trainingdata[, -4]
+test = validationdata[, -4]
+
+churnfactor= factor(train$Churn.)
+churnfactor2=factor(test$Churn.)
+test2=data.frame(test, churnfactor2)
+
+rf2 =randomForest(churnfactor ~ .,data=train, method="class")
+
+pred3=predict(rf2, test2, type="class")
+print(table(pred3, test2[,"churnfactor2"]))
